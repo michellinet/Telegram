9b9bf1bd48db7d6fed7f5107202bfc9dc4a1f4d0
Michelline Tran <michellinet@procore.com>
2017-07-06 14:22:33 -0700

    Set up UI.

--- Telegram/ViewController.swift
+++ Telegram/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var wrapper = Wrapper()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        wrapper.maxCharacters = 11
+        wrapper.text = ["This is a cat.", "Meow."]
+
+        self.tableView.separatorStyle = .none
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wrapper.wrapText().count
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = wrapper.wrapText()[indexPath.row]
+        return cell
+    }
 }
-
